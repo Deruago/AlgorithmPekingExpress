e5cc73707160615e579eb9a053edfe654f309d78
Vliet,Matthijs M.B. van <matthijs.vanvliet@student.fontys.nl>
2021-03-03 10:54:56 +0100

    Updated GameUpdate.h and GameUpdate.cpp

---
+++ include/PekingExpress/Game/Update/GameUpdate.h
@@ -0,0 +1,22 @@
+#ifndef PEKINGEXPRESS_GAME_UPDATE_GAMEUPDATE_H
+#define PEKINGEXPRESS_GAME_UPDATE_GAMEUPDATE_H
+
+#include "PekingExpress/Game/Model/IGraph.h"
+#include "PekingExpress/Game/Model/ICouple.h"
+
+namespace PekingExpress
+{
+	class GameUpdate
+	{
+	public:
+		GameUpdate(IGraph& graph, ICouple& ourCouple, ICouple& competitor) = default;
+		~GameUpdate() = default;
+
+		void UpdateCompetitorLocation(int location);
+		int NextMove();
+	private:
+		IGraph& graph;
+	};
+}
+
+#endif //PEKINGEXPRESS_GAME_UPDATE_GAMEUPDATE_H
---
+++ lib/Game/Update/GameUpdate.cpp
@@ -0,0 +1,16 @@
+#include "PekingExpress/Game/Update/GameUpdate.h"
+
+PekingExpress::GameUpdate::GameUpdate(IGraph& graph, ICouple& ourCouple, ICouple& competitor)
+{
+
+}
+
+void PekingExpress::GameUpdate::UpdateCompetitorLocation(int location)
+{
+
+}
+
+int PekingExpress::GameUpdate::NextMove()
+{
+	return 0;
+}
