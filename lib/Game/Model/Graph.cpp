7345b1ae804556f00e1db6efbdb21c034e532c88
Thimo Bohmer <thimo@bohmer.net>
2021-03-24 16:37:52 +0100

    Add main application

--- lib/Game/Model/Graph.cpp
+++ lib/Game/Model/Graph.cpp
@@ -18,6 +18,35 @@ const std::vector<PekingExpress::Node*> PekingExpress::Graph::GetLocations() con
 	return locations;
 }
 
+PekingExpress::Node* PekingExpress::Graph::GetNode(int id) const
+{
+	for (auto node : locations)
+	{
+		if (node->GetId() == id)
+		{
+			return node;
+		}
+	}
+	return nullptr;
+}
+
+std::vector<PekingExpress::Node*> PekingExpress::Graph::GetNodes(const std::vector<int>& ids) const
+{
+	std::vector<Node*> nodes = {};
+	for (auto id : ids)
+	{
+		for (auto node : locations)
+		{
+			if (node->GetId() == id)
+			{
+				nodes.push_back(node);
+			}
+		}
+	}
+	
+	return nodes;
+}
+
 const std::vector<PekingExpress::Node*> PekingExpress::Graph::GetCriticalLocations() const
 {
 	std::vector<PekingExpress::Node*> criticals;
--- main.cpp
+++ main.cpp
@@ -1,9 +1,102 @@
 #include <iostream>
-#include "PekingExpress/testa.h"
+#include "PekingExpress/Game/Init/JsonToGraph.h"
+#include "PekingExpress/Game/Update/GameUpdate.h"
+#include <fstream>
 
+bool OpponentsAtEnding(const std::vector<std::vector<PekingExpress::Node*>>& vector, int currentLocation)
+{
+	for (auto* i : vector[currentLocation])
+	{
+		if (i->GetId() == 88)
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
 int main()
 {
-	Testa::Print();
-	std::cout << "Hallo" << std::endl;
+	std::ifstream input;
+	input.open("./data.json");
+	
+	std::string inputString;
+	std::string line;
+
+	while (getline(input, line))
+	{
+		inputString += line;
+	}
+
+	std::cout << "Input startlocation: ";
+	int startlocation = 0;
+	std::cin >> startlocation;
+	
+	std::cout << "Input budget: ";
+	int budget = 0;
+	std::cin >> budget;
+	
+	PekingExpress::JsonToGraph graph;
+	auto* newGraph = graph.ConvertJsonToGraph(inputString);
+
+	auto couple1 = new PekingExpress::Couple(budget, 1, newGraph->GetNode(startlocation));
+	auto couple2 = new PekingExpress::Couple(budget, 2, newGraph->GetNode(startlocation));
+
+	PekingExpress::GameUpdate gameUpdate(newGraph, couple1, { couple2 });
+
+	//std::vector<PekingExpress::Move*> path;
+	std::vector<int> path;
+
+	std::vector<std::vector<PekingExpress::Node*>> opponentsMove = {
+		{
+			newGraph->GetNodes({2, 3})
+		},
+		{
+			newGraph->GetNodes({3})
+		},
+		{
+			newGraph->GetNodes({88})
+		},
+		{
+			newGraph->GetNodes({88})
+		}
+	};
+
+	int currentLocation = 0;
+
+	bool win = false;
+	
+	while (true)
+	{
+		auto move = gameUpdate.NextMove();
+		path.push_back(move);
+		gameUpdate.ApplyOurMove(move);
+		if (gameUpdate.CanOpponentMove(opponentsMove[currentLocation + 1]))
+		{
+			if ((currentLocation + 1) < opponentsMove.size())
+			{
+				currentLocation += 1;			
+			}
+		}
+		
+		gameUpdate.UpdateOccupiedLocations({ opponentsMove[currentLocation], move });
+		if (move == 88)
+		{
+			win = true;
+			break;
+		}
+		else if (OpponentsAtEnding(opponentsMove, currentLocation))
+		{
+			win = false;
+			break;
+		}
+	}
+
+	std::cout << "Path taken:\n";
+	for (auto i : path)
+	{
+		std::cout << i << " ";
+	}
+
+	std::cout << '\n';
 }
